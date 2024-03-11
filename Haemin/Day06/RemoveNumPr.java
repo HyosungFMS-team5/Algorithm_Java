bff1048f2f5d16d2e042b808558a18bd71e621fe
hyehmin <qqqwe10000@likelion.org>
2024-03-11 17:10:12 +0900

    Day06 알고리즘 문제 올립니다!!

---
+++ Haemin/Day06/RemoveNumPr.java
@@ -0,0 +1,30 @@
+package alonestudy;
+
+import java.util.Scanner;
+
+public class RemoveNumPr {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] num = new int[101];
+		boolean arr[] = new boolean[101];
+		
+		
+		for(int i = 0; i < num.length; i++) {
+			num[i] = sc.nextInt();
+			
+			if(arr[i] = true) {
+				continue;
+			}
+			arr[i] = true;
+			
+		}
+		
+		for(int j = 0; j < num.length; j++) {
+			System.out.println(num[j] + " ");
+		}
+
+	}
+
+}
---
+++ Haemin/Day06/ThreesixninePr.java
@@ -0,0 +1,36 @@
+package alonestudy;
+
+public class ThreesixninePr {
+	
+	public static int getNum(int i ) {
+		int j = 0;
+		
+		while(i > 0) {
+			if(i%10 == 3 || i%10 == 6 || i%10 == 9) {
+				j++;
+			}
+			i /=10;
+		}
+		
+		return j;
+	}
+	
+	public static void main(String[] args) {
+		int n = 100;
+		
+		for(int i =1; i <= n; i++) {
+			int cnt = getNum(i);
+			
+			if(cnt == 0) {
+				System.out.print(i+" ");
+			}else {
+				for(int l = 0; l < cnt; l++) {
+					System.out.print("짝");
+				}
+				System.out.print(" ");
+			}
+		}
+
+	}
+
+}
